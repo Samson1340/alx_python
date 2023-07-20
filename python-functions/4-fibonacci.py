8b892f11bcd4dbcaef7a4b6b494117d370ec2587
Samson1340 <samsonsimeon2004@gmail.com>
2023-07-20 16:27:29 +0300

    update

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,15 +1,10 @@
 def fibonacci_sequence(n):
-    if n<= 0:
-        print("Incorrect input")
-    # First Fibonacci number is 0
-    elif n == 1:
+    if n < 0:
+        return ''
+    elif n == 0:
         return 0
-    # Second Fibonacci number is 1
-    elif n == 2:
+    elif n == 1 or n == 2:
         return 1
     else:
-        return fibonacci_sequence(n-1)+fibonacci_sequence(n-2)
- 
-# Driver Program
- 
-print(fibonacci_sequence(5))
+        return fibonacci_sequence(n-1) + fibonacci_sequence(n-2)
+    
