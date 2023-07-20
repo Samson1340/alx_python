a17846c4835e8803394e0a64d173498fe4f1c6f6
Samson1340 <samsonsimeon2004@gmail.com>
2023-07-20 09:29:43 +0300

    update

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,9 +1,10 @@
 def is_prime(number):
     is_prime = True
-    if number <= 0:
+    if number == 1:
         is_prime = False
-    else:
+    elif number > 1:
         for x in range(2,number):
             if number % x == 0:
                 is_prime = False
+                break
     return is_prime
