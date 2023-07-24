18c737a553d8d2d85e918698c5d981633d0c4aea
Samson1340 <samsonsimeon2004@gmail.com>
2023-07-24 16:03:48 +0300

    update

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,17 +1,16 @@
 if __name__ == "__main__":
-    import sys
-    num = len(sys.argv) - 1
+    def argumentPrint(argv):
+        num = len(argv) - 1
+        if num == 0:
+            print("{} arguments.".format(num))
+        elif num == 1:
+            print("{} argument:".format(num))
+        else:
+            print("{} arguments:".format(num))
 
-    if num == 0:
-        print("{} arguments.".format(num))
-    elif num == 1:
-        print("{} argument:".format(num))
-    else:
-        print("{} arguments:".format(num))
-
-    if num >= 1:
-        num = 0
-        for arg in sys.argv:
-            if num != 0:
-                print("{}: {}".format(num, arg))
-            num += 1
+        if num >= 1:
+            num = 0
+            for arg in argv:
+                if num != 0:
+                    print("{}: {}".format(num, arg))
+                num += 1
