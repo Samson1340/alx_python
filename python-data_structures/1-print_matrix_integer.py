e07d96f21d938b746e73e6761b3e62ce36e2fcab
Samson1340 <samsonsimeon2004@gmail.com>
2023-07-26 12:37:40 +0300

    update

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,2 +1,9 @@
 def print_matrix_integer(matrix=[[]]):
-    [[row[i] for row in matrix] for i in range(3)]
+    if not matrix:
+        return None
+    for submatrix in matrix:
+        if len(submatrix) == 0:
+            print()
+            for i in range(len(submatrix)):
+                print("{:d}".format(submatrix[i]),
+                    end="\n" if i is len(submatrix) - 1 else " ")
