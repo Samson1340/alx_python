f56f41c741b2fb9c154d53cb108cf5764b3d7612
Samson1340 <samsonsimeon2004@gmail.com>
2023-07-26 10:57:17 +0300

    update

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -2,8 +2,7 @@ def safe_print_division(a, b):
     try:
         c = a / b
     except ZeroDivisionError:
-        print("Inside result: None")
-        print("{} / {} = None".format(a, b))
+        c = None
     finally:
         print("Inside result: {}".format(c))
         print("{} / {} = {}".format(a, b, c))
