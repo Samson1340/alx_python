ff1f387d370d7d9158efd3346b1f38d99efb59ac
Samson1340 <samsonsimeon2004@gmail.com>
2023-08-10 22:47:27 +0300

    update

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ Uses requests module. Prints error code"""
-from request import Request
+from requests import requests
 from requests import sys 
 
 if __name__ == "__main__":
-    response = Request.get(argv[1])
+    response = requests.get(argv[1])
     if response.status_code > 400:
         print("Error code:", response.status_code)
     else:
