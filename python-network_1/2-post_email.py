e9311337fbbb4bc6a417c451b4495b1789fd0425
Samson1340 <samsonsimeon2004@gmail.com>
2023-08-10 22:54:12 +0300

    update

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
-""" Uses requests module to get header info"""
+"""Sends a POST request to a given URL with a given email.
+
+Usage: ./6-post_email.py <URL> <email>
+  - Displays the body of the response.
+"""
+import sys
 import requests
-from sys import argv
+
 
 if __name__ == "__main__":
-    response = requests.post(argv[1], data={'email': argv[2]})
-    print(response.text)
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    r = requests.post(url, data=value)
+    print(r.text)
