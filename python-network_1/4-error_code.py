e38557c6d96bf5254f6f941f27cdd5f1f64259bb
Samson1340 <samsonsimeon2004@gmail.com>
2023-08-10 22:45:37 +0300

    update

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Uses requests module. Prints error code"""
-from urllib.request import Request
-from sys import argv
+from request import Request
+from requests import sys 
 
 if __name__ == "__main__":
     response = Request.get(argv[1])
