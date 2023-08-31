76d64ba31c2b50d491fbbc59472418b362d7a6c2
Samson1340 <samsonsimeon2004@gmail.com>
2023-08-31 15:10:29 +0300

    Update

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,17 @@
+"""
+This is to start Flask web application including route
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def Hello_HBNB():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
