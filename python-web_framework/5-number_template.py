8b15ed95989996a48c4097e0ad104e3dfd87d39f
Samson1340 <samsonsimeon2004@gmail.com>
2023-08-31 16:23:56 +0300

    update

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -24,7 +24,7 @@ def pythoniscool(text='is cool'):
 
 @app.route('/number_template/<n>',strict_slashes=False)
 def number_template(n):
-    return render_template('5-number.html', Number=n)
+    return render_template('5-number.html', n=n)
         
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
