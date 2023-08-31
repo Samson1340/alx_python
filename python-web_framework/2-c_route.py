e21f52ec68f4960083f19401ddb87ae47461da82
Samson1340 <samsonsimeon2004@gmail.com>
2023-08-31 15:35:16 +0300

    Update

---
+++ python-web_framework/2-c_route.py
@@ -0,0 +1,21 @@
+"""
+This is to start Flask web application including route
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def Hello_HBNB():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    return 'C' + text.replace('_', '')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
