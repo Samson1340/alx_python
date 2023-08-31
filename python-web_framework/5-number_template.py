ce0061fc0bc5533e550e4d550f9472c3d380e046
Samson1340 <samsonsimeon2004@gmail.com>
2023-08-31 16:17:02 +0300

    update

---
+++ python-web_framework/5-number_template.py
@@ -0,0 +1,30 @@
+"""
+This is to start Flask web application including route
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def Hello_HBNB():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    return 'C ' + text.replace('_', ' ')
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    return 'Python ' + text.replace('_', ' ')
+
+@app.route('/number_template/<n>',strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', Number=n)
+        
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
