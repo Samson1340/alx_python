41d9fc1abb2e781e571e71cd22d08511168484bf
Samson1340 <samsonsimeon2004@gmail.com>
2023-08-31 14:43:49 +0300

    update

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,7 +1,14 @@
-from flask import Flask
+"""
+this is to start a Flask web application
+"""
 
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/', strict_slashes=False)
 def Hello_HBNB():
-    return "<p>Hello HBNB!</p>"
+    """it returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
