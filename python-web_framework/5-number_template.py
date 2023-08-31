8e42ea98c971f12f2a2ad6f97de90ef024b312fe
Samson1340 <samsonsimeon2004@gmail.com>
2023-08-31 16:27:53 +0300

    update

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -22,8 +22,12 @@ def cisfun(text):
 def pythoniscool(text='is cool'):
     return 'Python ' + text.replace('_', ' ')
 
-@app.route('/number_template/<n>',strict_slashes=False)
-def number_template(n):
+@app.route('/number/<int:n>',strict_slashes=False)
+def nisinteger(n):
+    return "{:d} is a number".format(n)
+
+@app.route('/number_template/<int:n>',strict_slashes=False)
+def imanumber(n):
     return render_template('5-number.html', n=n)
         
 if __name__ == '__main__':
