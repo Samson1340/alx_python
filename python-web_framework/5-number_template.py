b99df3d67cffe307589092bd9cbd057db9adb6d7
Samson1340 <samsonsimeon2004@gmail.com>
2023-08-31 16:38:24 +0300

    Update

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -29,7 +29,7 @@ def nisinteger(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def numberstemplates(n):
     """display a HTML page only if n is an integer"""
-    return render_template('5-number.html', n=n)
+    return render_template('5-number.html', n="{:d}".format(n))
         
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
