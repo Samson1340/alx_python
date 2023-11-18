f3ce24647f0ea0c2a58c068b1e4ca9d1d3e3ab24
Samson1340 <samsonsimeon2004@gmail.com>
2023-11-18 12:45:53 +0300

    update

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,46 +1,25 @@
-# this is the todo list api
+#!/usr/bin/python3
+"""gets api"""
 import requests
-import sys
+from sys import argv
 
-def get_employee_info(employee_id):
-    # Endpoint for employee details
-    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    employee_response = requests.get(employee_url)
 
-    if employee_response.status_code == 200:
-        employee_data = employee_response.json()
-        return employee_data['name']
-    else:
-        print(f"Error fetching employee details for ID {employee_id}")
-        sys.exit(1)
+def todo(userid):
+    """doc stringed"""
+    name = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(
+            userid)).json().get('name')
+    tasks = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+            userid)).json()
+    tasksDone = ['\t {}\n'.format(dic.get('title')) for dic in tasks
+                 if dic.get('completed')]
+    if name and tasks:
+        print("Employee {} is done with tasks({}/{}):".format
+              (name, len(tasksDone), len(tasks)))
+        print(''.join(tasksDone), end='')
 
-def get_todo_progress(employee_id):
-    # Endpoint for employee's TODO list
-    todo_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-    todo_response = requests.get(todo_url)
-
-    if todo_response.status_code == 200:
-        todo_data = todo_response.json()
-
-        # Count completed and total tasks
-        completed_tasks = [task for task in todo_data if task['completed']]
-        total_tasks = len(todo_data)
-
-        # Display employee TODO list progress
-        employee_name = get_employee_info(employee_id)
-        print(f"Employee {employee_name} is done with tasks ({len(completed_tasks)}/{total_tasks}):")
-
-        # Display titles of completed tasks
-        for task in completed_tasks:
-            print(f"\t{task['title']}")
-    else:
-        print(f"Error fetching TODO list for employee ID {employee_id}")
-        sys.exit(1)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python employee_todo.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-    get_todo_progress(employee_id)
+    if len(argv) == 2:
+        todo(int(argv[1]))
