0e3070d786f2353fba35ae771ba9d46a292323bb
Samson1340 <samsonsimeon2004@gmail.com>
2023-11-18 12:54:47 +0300

    update

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
-# csv exported
+"""
+    Uses the fake API to get an employer and export the info in json formater
+"""
 import json
-from requests import get
+import requests
 from sys import argv
 
-
-def jsonWrite(user):
-    """writes to csv"""
-    data = get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(
-        user)).json()
-    name = get('https://jsonplaceholder.typicode.com/users/{}'.format(
-        user)).json().get('username')
-    ordered = []
-    for line in data:
-        ordered.append({"task": line.get('title'), "completed":
-                        line.get('completed'), "username": name})
-    with open('{}.json'.format(user), 'w') as f:
-        json.dump({user: ordered}, f)
-
-
 if __name__ == "__main__":
-    jsonWrite(int(argv[1]))
+    id_em = argv[1]
+    url_employ = "https://jsonplaceholder.typicode.com/users/{}".format(id_em)
+    url_todos = url_employ + "/todos"
+    r_employ = requests.get(url_employ).json()
+    r_todos = requests.get(url_todos).json()
+    username = r_employ.get("username")
+    total_num_task = r_todos
+    list_dict_report = []
+    for task in total_num_task:
+        id_report = {}
+        id_report["username"] = str(username)
+        id_report["completed"] = task.get("completed")
+        id_report["task"] = str(task.get("title"))
+        list_dict_report.append(id_report)
+    report = {}
+    report[id_em] = list_dict_report
+    with open("{}.json".format(id_em), "w") as fjson:
+        fjson.write(json.dumps(report))
